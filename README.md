7a604302857534c2a85d488b0bd1532109178d04
Nathanael Anderson <nathan@master-technology.com>
2016-02-17 21:44:07 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# nativescript-dom
+A class of DOM based NativeScript functions
+
+## License
+
+This is released under the MIT License, meaning you are free to include this in any type of program -- However for entities that need a support contract, changes, enhancements and/or a commercial license please contact me (nathan@master-technology.com).
+
+## Updates
+
+Please feel free to fork this repo and update the functions or add additional DOM based functions!!!
+
+
+## Installation 
+
+tns plugin add nativescript-dom  
+
+
+## Usage
+
+To use the  module you just `require()` it:
+
+```js
+require( "nativescript-dom" );
+```
+
+Notice: You do NOT need to keep a reference to it; and you only need to load it once.  
+It will automatically attach its methods to all the proper classes in the NativeScript library making it act as if they are built in.
+
+ 
+### Methods
+
+#### getElementById(id)
+#### getElementsByClassName(className)
+#### getElementsByTagName(tagName)
+These are globally available!  Like there Web DOM counterparts; they return elements based on the critera.
+```js
+  var element = getElementById('someId');
+```
+
+#### view.getElementById(id)
+#### view.getElementsByClassName(className)
+#### view.getElementsByTagName(tagName)
+Like there Web DOM counterparts; returns the children elements based on the critera.
+```js
+exports.pageLoaded = function(args) {
+  var page = args.object;
+  var stackLayout = page.getElementsTagName('StackLayout')[0];
+  var button = stackLayout.getElementsByClassName('clickButton')[0];
+  button.classList.toggle('hidden');
+}
+```
+
+
+#### view.classList.add(className)
+Add a class to the view's class list at the end
+```js
+   someButton.classList.add('hidden');  // ClassList on this button will be "class1 class2 classx hidden"
+```
+
+#### view.classList.insert(className)
+Add a class to the view's class list at the front
+```js
+   someButton.classList.insert('hidden'); // ClassList on this button will be "hidden class1 class2 classx"
+```
+
+#### view.classList.remove(className)
+Removes a class from the view's class list
+```js
+   someButton.classList.remove('hidden'); // ClassList would then equal "class1 class2 class3"
+```
+   
+#### view.classList.toggle(className[, force])
+Toggles a class name
+if force = true, will force adding the class name only.     (And won't remove it, but you won't have a second)
+if force = false, will force removing the class name only.  (And won't add it)
+
+#### view.classList.item(id) 
+Returns the class name at that location.
+
+#### view.classList.contains(className)
+Returns true or false if the class name exists in the class list.
+```js
+  if (someButton.contains('hidden')) {
+     someButton.remove('hidden');
+  } else {
+     someButton.add('hidden');
+  }
+
+  // Although   someButton.toggle('hidden');    would be equivelent to the 5 lines above.
+```
+
---
+++ dom.js
@@ -0,0 +1,261 @@
+/**********************************************************************************
+ * (c) 2015-2016, Master Technology
+ * Licensed under the MIT license or contact me for a Support or Commercial License
+ *
+ * I do contract work in most languages, so let me solve your problems!
+ *
+ * Any questions please feel free to email me or put a issue up on the github repo
+ * Version 0.0.2                                      Nathan@master-technology.com
+ *********************************************************************************/
+"use strict";
+
+/* jshint node: true, browser: true, unused: true, undef: true */
+/* global android, com, java, javax, exit, UIDevice, CFAbsoluteTimeGetCurrent, NSRunLoop, NSDate */
+
+
+// Load the required modules
+var view = require('ui/core/view');
+var frame = require('ui/frame');
+
+
+// global.android is already defined on android devices
+// We are defining global.ios on ios devices
+if (global.NSObject && global.NSString && typeof global.ios === "undefined") {
+    global.ios = true;
+    Object.freeze(global.ios);
+}
+
+if (!global.getElementById) {
+    /***
+     * Find a element by an id
+     * @param id
+     * @returns {view} or {undefined}
+     */
+    global.getElementById = function (id) {
+        return view.getViewById(getCurrentActiveModel(), id);
+    };
+}
+
+if (!view.View.prototype.getElementById) {
+    /***
+     * Find an element by a id
+     * @param id
+     * @returns {view} or {undefined}
+     */
+    view.View.prototype.getElementById = function (id) {
+        return view.getViewById(this, id);
+    };
+}
+
+if (!global.getElementsByClassName) {
+    /***
+     * getElementsByClassName
+     * @param className - The class name
+     * @returns {Array} of elements
+     */
+    global.getElementsByClassName = function (className) {
+        return getElementsByClassName(getCurrentActiveModel(), className);
+    };
+}
+
+if (!view.View.prototype.getElementsByClassName) {
+    /***
+     * Finds all elements with the class name
+     * @param className - the Class name
+     * @returns {Array} of elements
+     */
+    view.View.prototype.getElementsByClassName = function (className) {
+        return getElementsByClassName(this, className);
+    };
+}
+
+if (!global.getElementsByTagName) {
+    /**
+     * Finds all elements by a Tag name
+     * @param tagName
+     * @returns {Array}
+     */
+    global.getElementsByTagName = function (tagName) {
+        return getElementsByTagName(getCurrentActiveModel, tagName);
+    };
+}
+
+if (!view.View.prototype.getElementsByTagName) {
+    /**
+     * Finds all elements by a Tag name
+     * @param tagName
+     * @returns {Array}
+     */
+    view.View.prototype.getElementsByTagName = function (tagName) {
+        return getElementsByTagName(this, tagName);
+    };
+}
+
+if (!view.View.prototype.classList) {
+    var classList = function(t) {
+        var curClassList = "";
+        this._resync = function() {
+            if (curClassList === t.cssClass) { return; }
+            var cls = t._cssClasses;
+            var len = cls.length;
+
+            // We need to zero our length; so that we can re-add anything that exists in the parent class
+            this.length = 0;
+            for (var i = 0; i < len; i++) {
+                if (!this.contains(cls[i])) {
+                    this.push(cls[i]);
+                }
+            }
+        };
+        this._update = function () {
+            t.cssClass = this.join(" ");
+            curClassList = t.cssClass;
+        };
+
+        this._resync();
+    };
+    classList.prototype = [];
+    classList.prototype.toString = function() {
+        this._resync();
+        return this.join(" ");
+    };
+    classList.prototype.item = function(i) {
+        this._resync();
+        return this[i] || null;
+    };
+    classList.prototype.add = function() {
+        this._resync();
+        var updated=false;
+        for (var i=0,len=arguments.length;i<len;i++) {
+            if (!this.contains(arguments[i])) {
+                this.push(arguments[i]);
+                updated = true;
+            }
+        }
+        if (updated) {
+            this._update();
+        }
+        return this;
+    };
+    classList.prototype.insert = function() {
+        this._resync();
+        var updated = false;
+        for (var i=0,len=arguments.length;i<len;i++) {
+            if (!this.contains(arguments[i])) {
+                this.unshift(arguments[i]);
+                updated = true;
+            }
+        }
+        if (updated) {
+            this._update();
+        }
+        return this;
+    };
+
+    classList.prototype.remove = function() {
+        this._resync();
+        var updated = false;
+        for (var i= 0,len=arguments.length;i<len;i++) {
+            var idx = this.indexOf(arguments[i]);
+            if (idx >= 0) {
+                this.splice(idx, 1);
+                updated = true;
+            }
+        }
+        if (updated) {
+            this._update();
+        }
+        return this;
+    };
+    classList.prototype.toggle = function(val, force) {
+        this._resync();
+        if (this.contains(val)) {
+            if (force === true) { return this; }
+            return this.remove(val);
+        } else {
+            if (force === false) { return this; }
+            return this.add(val);
+        }
+    };
+    classList.prototype.contains = function(c) {
+            return this.indexOf(c) >= 0;
+    };
+    var getClassList = function (val) {
+        var cl = new classList(val);
+        Object.defineProperty(val, "classList", { value: cl, configurable: true, enumerable: true });
+        return cl;
+    };
+    Object.defineProperty(view.View.prototype, "classList", {configurable: true, enumerable: true, get: function() { return getClassList(this); }});
+}
+
+
+/*** Support routines, not publicly accessible ***/
+function getElementsByClassName(v, clsName) {
+    var retVal=[];
+    if (!v) {
+        return retVal;
+    }
+
+    if (v._cssClasses && v._cssClasses.length && v._cssClasses.indexOf(clsName) !== -1) {
+        retVal.push(v);
+    }
+
+    var classNameCallback = function (child) {
+        if (child._cssClasses && child._cssClasses.length && child._cssClasses.indexOf(clsName) !== -1) {
+            retVal.push(child);
+        }
+        
+        // Android patch for ListView
+        if (child._realizedItems) {
+            for (var key in child._realizedItems) {
+                if (child._realizedItems.hasOwnProperty(key)) {
+                    classNameCallback(child._realizedItems[key]);
+                }
+            }
+        }
+
+        return true;
+    };
+
+    view.eachDescendant(v, classNameCallback);
+
+    // Android patch for ListView
+    if (v._realizedItems) {
+        for (var key in v._realizedItems) {
+            if (v._realizedItems.hasOwnProperty(key)) {
+                classNameCallback(v._realizedItems[key]);
+            }
+        }
+    }
+
+
+    return retVal;
+}
+
+function getElementsByTagName(v, tagName) {
+    var retVal=[];
+    if (!v) {
+        return retVal;
+    }
+
+    if (v.typeName && v.typeName === tagName) {
+        retVal.push(v);
+    }
+
+    var tagNameCallback = function (child) {
+        if (child.typeName === tagName) {
+            retVal.push(child);
+        }
+        return true;
+    };
+
+    view.eachDescendant(v, tagNameCallback);
+    return retVal;
+}
+
+var getCurrentActiveModel = function() {
+    var topFrame = frame.topmost();
+    var model = topFrame.currentPage && topFrame.currentPage.model;
+    if (model) { return model; }
+    return topFrame;
+};
